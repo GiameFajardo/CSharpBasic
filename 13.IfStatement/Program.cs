8fef5b076ea4c25d75d6e0d68fb445a2fde4a455
Giame Fajardo <gmcrls13@gmail.com>
2022-05-07 10:46:14 -0400

    If and Switch statement

---
+++ 13.IfStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _13.IfStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anyValue = 4;
+            string message = "";
+            if (anyValue == 13)
+            {
+                message = "The value is 13!";
+
+            } else if (anyValue > 13)
+            {
+                message = "The value greater than 13!";
+
+            }
+            else
+            {
+                message = "The value is less than 13.";
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ 14.SwitchStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14.SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string option = "";
+            option = Console.ReadLine();
+            switch (option)
+            {
+                case "naranja":
+                    Console.WriteLine("$20");
+                    break;
+                case "fresa":
+                    Console.WriteLine("$35");
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid Value.");
+                    break;
+            }
+        }
+    }
+}
